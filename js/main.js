f540c8b548b3d6cd9ddcf548c0ff8423d3a4824e
mohamed9167215 <83306599+mohamed9167215@users.noreply.github.com>
2022-06-13 16:22:54 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.top-bar .toggle span').click(function(){
+          $('.navigation').toggleClass('actives');
+        $('.main').toggleClass('active');
+     //   $('.main').css( "background" , "red");//
+
+
+    });
+ 
+
+
+});
